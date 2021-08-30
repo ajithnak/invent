621583b8c6a1f1c2355c0ef58ae777b20a18ebd2
ajithnak <ajithindia2050@gmail.com>
2021-08-30 13:57:02 +0530

    hash

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import Cooking from './Cooking';
